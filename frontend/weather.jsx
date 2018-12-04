1748793f4ed50e701f059bc870b515c5aedce21e
Kahaan <kahaan.r.patel@gmail.com>
2018-12-04 14:44:22 -0800

    weather widget

--- frontend/weather.jsx
+++ frontend/weather.jsx
@@ -12,10 +12,10 @@ class Weather extends React.Component{
     navigator.geolocation.getCurrentPosition(function(position) {
       this.lat = position.coords.latitude
       this.lon = position.coords.longitude
-    }, console.log(this.lat, this.lon, 'sent'));
+    // }, console.log(this.lat, this.lon, 'sent'));
   }
 
-  // 
+  //
   // var request = new XMLHttpRequest();
   //   request.open('GET', '', true);
   //
